6feea4a163b53be3f85bd576afe8be8865995f85
Quinn Wood <59754839+qskwood@users.noreply.github.com>
2025-01-16 03:18:35 -0600

    feat: add prompt
    The program did not tell the user to provide input. Now it does.

--- src/basic-io.c
+++ src/basic-io.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+#define PROMPT_ENUS "Please provide some input...\n"
+
 int main() {
     /* define a variable in which to store user-provided characters */
     int character;
+
+    printf(PROMPT_ENUS);
 
     /* get input from the user and store it in a variable */
     /* loop unless the user last provided EOF */
