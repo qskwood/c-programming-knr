98b02c60741e14dfddfc8963a390583df229a0c0
Quinn Wood <59754839+qskwood@users.noreply.github.com>
2025-01-16 03:39:22 -0600

    feat: add character counter
    The program now tells the user how many characters were input.

--- src/basic-io.c
+++ src/basic-io.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 
 #define PROMPT_ENUS "Please provide some input...\n"
+#define CHAR_COUNT_TEXT_ENUS "You entered the following number of characters"
 
 int main() {
     /* define a variable in which to store user-provided characters */
     int character;
+    /* define a variable in which to store the character count as it grows */
+    long character_count;
+    /* set the initial character count to zero */
+    character_count = 0;
 
     printf(PROMPT_ENUS);
 
@@ -13,5 +18,9 @@ int main() {
     while ((character = getchar()) != EOF) {
         /* output the character the user provided */
         putchar(character);
+        ++character_count;
     }
+
+    /* tell the user how many characters they input */
+    printf("%s: %d\n", CHAR_COUNT_TEXT_ENUS, character_count);
 }
